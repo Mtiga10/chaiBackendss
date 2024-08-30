f7454d271d9ddb3f17a00cff88b8489a9fa4826e
Tenzz <tennam2507@gmail.com>
2024-08-30 19:28:09 +0200

    github data added

--- index.js
+++ index.js
@@ -3,6 +3,41 @@ const express = require('express')
 //import express from "express" // we can also import in this way
 const app = express()
 const port = 4000
+const githubData = {
+    "login": "Mtiga10",
+    "id": 37522216,
+    "node_id": "MDQ6VXNlcjM3NTIyMjE2",
+    "avatar_url": "https://avatars.githubusercontent.com/u/37522216?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/Mtiga10",
+    "html_url": "https://github.com/Mtiga10",
+    "followers_url": "https://api.github.com/users/Mtiga10/followers",
+    "following_url": "https://api.github.com/users/Mtiga10/following{/other_user}",
+    "gists_url": "https://api.github.com/users/Mtiga10/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/Mtiga10/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/Mtiga10/subscriptions",
+    "organizations_url": "https://api.github.com/users/Mtiga10/orgs",
+    "repos_url": "https://api.github.com/users/Mtiga10/repos",
+    "events_url": "https://api.github.com/users/Mtiga10/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/Mtiga10/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": null,
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 11,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 0,
+    "created_at": "2018-03-19T01:28:03Z",
+    "updated_at": "2024-08-30T12:51:34Z"
+    }
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -13,7 +48,9 @@ app.get('/help',(req,res)=>{
 app.get('/email',(req,res)=>{
     res.send('Enter your Email Id')
 })
-
+app.get('/github',(req,res)=>{
+    res.json(githubData)
+})
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
